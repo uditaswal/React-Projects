567daf86aace94a0f0d355c63582d0aaa738994f
udit aswal <udit.aswal37@gmail.com>
2024-11-01 02:33:45 +0530

    Initial commit

---
+++ 09DarkModeToggle/src/context/UserContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { unload } from '../../node_modules/signal-exit/dist/mjs/browser';
+
+const UserContext=React.createContext();
+
+export default UserContext;
+
+// to create provider
+{/* <UserContext>
+<Login/>
+</UserContext> */}
