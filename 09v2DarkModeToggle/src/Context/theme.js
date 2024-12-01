d54535e95f8693279b408dd385dd1d631fc59ca5
udit aswal <udit.aswal37@gmail.com>
2024-12-01 14:36:08 +0530

    complete

---
+++ 09v2DarkModeToggle/src/Context/theme.js
@@ -0,0 +1,17 @@
+import React , {Children, createContext,useContext} from "react";
+ export const ThemeContext=createContext({
+
+themeMode:"light",
+darkTheme : () =>{
+
+},
+lightTheme : () =>{
+}
+
+
+ });
+export const ThemeProvider=ThemeContext.Provider;
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
